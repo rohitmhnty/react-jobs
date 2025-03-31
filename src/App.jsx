0da2ef3501a80655bccd6b5a09bba290fcd424e8
rohitmhnty <rohitkumarmahanty1234@gmail.com>
2025-04-01 01:04:47 +0530

    completed upto Conditional Rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,34 @@
-import React from 'react'
-import Navbar from './components/Navbar'
-import Hero from './components/Hero'
-import HomeCards from './components/HomeCards'
-import JobListings from './components/JobListings'
-import ViewAllJobs from './components/ViewAllJobs'
+import React from 'react';
+import {Route, createBrowserRouter, RouterProvider} from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import MainLayout from './layouts/MainLayout';
+import JobsPage from './pages/JobsPage';
+import NotFoundPage from './pages/NotFoundPage';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      {
+        path: "/",
+        element: <HomePage />,
+      },
+      {
+        path: "/jobs",
+        element: <JobsPage />,
+      },
+      {
+        path: "*",
+        element: <NotFoundPage />,
+      },
+    ],
+  },
+]);
 
 const App = () => {
 
-  return (
-    <>
-    <Navbar />
-    <Hero />
-    <HomeCards />
-    <JobListings />
-    <ViewAllJobs /> 
-    </>
-  )
-}
+  return <RouterProvider router={router} />;
+};
 
-export default App
+export default App;
