ce7fa8081664fc2a9dcd00b2396a26c341de526c
rohitmhnty <rohitkumarmahanty1234@gmail.com>
2025-04-09 02:37:18 +0530

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,29 +4,83 @@ import HomePage from './pages/HomePage';
 import MainLayout from './layouts/MainLayout';
 import JobsPage from './pages/JobsPage';
 import NotFoundPage from './pages/NotFoundPage';
+import JobPage, {jobLoader} from './pages/JobPage';
+import AddJobPage from './pages/AddJobPage';
+import EditJobPage from './pages/EditJobPage';
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <MainLayout />,
-    children: [
-      {
-        path: "/",
-        element: <HomePage />,
-      },
-      {
-        path: "/jobs",
-        element: <JobsPage />,
-      },
-      {
-        path: "*",
-        element: <NotFoundPage />,
-      },
-    ],
-  },
-]);
+
 
 const App = () => {
+  // Add new job
+  const addJob = async (newJob) => {
+    const res = await fetch('/api/jobs', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newJob),
+  });
+    return;
+
+  };
+
+  //Delete job
+  const deleteJob = async (id) => {
+      const res = await fetch(`/api/jobs/${id}`, {
+        method: 'DELETE',
+    });
+      return;
+  
+  };
+
+  //Update job
+  const updateJob = async(job) => {
+    const res = await fetch(`/api/jobs/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(job),
+  });
+    return;
+
+  }
+
+  
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <MainLayout />,
+      children: [
+        {
+          path: "/",
+          element: <HomePage />,
+        },
+        {
+          path: "/jobs",
+          element: <JobsPage />,
+        },
+        {
+          path: "*",
+          element: <NotFoundPage />,
+        },
+        {
+          path: "/jobs/:id",
+          element: <JobPage deleteJob = {deleteJob}/>,
+          loader: jobLoader,
+        },
+        {
+          path: "/edit-job/:id",
+          element: <EditJobPage updateJobSubmit={updateJob}/>,
+          loader: jobLoader,
+        },
+        {
+          path: '/add-job',
+          element: <AddJobPage addJobSubmit= {addJob}/>,
+        },
+      ],
+    },
+  ]);
 
   return <RouterProvider router={router} />;
 };
--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -1,8 +1,27 @@
-import jobs from '../jobs.json'
+import {useState, useEffect} from 'react';
 import JobListing from './JobListing';
+import Spinner from './Spinner';
 
 const JobListings = ({isHome = false}) => {
-const jobListings  = isHome? jobs.slice(0,3) : jobs;
+  const [jobs, setJobs] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const apUrl = isHome? '/api/jobs?_limit=3': '/api/jobs';
+    const fetchJobs = async () => {
+      try{
+        const res = await fetch(apUrl);
+        const data = await res.json();
+        setJobs(data);
+      } catch(error){
+        console.log('Error fetching data', error);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchJobs();
+  }, []);
 
   return (
     <section className="bg-blue-50 px-4 py-10">
@@ -10,13 +29,17 @@ const jobListings  = isHome? jobs.slice(0,3) : jobs;
         <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
           { isHome? 'Recent Jobs' : 'Browse Jobs'}
         </h2>
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+        
+          { loading? (
+            <Spinner loading= {loading} />
+          ): (
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
             {
-                jobListings.map((job) =>(
+                jobs.map((job) =>(
                     <JobListing key={job.id} job= {job} />
                     ))}
-          
-        </div>
+            </div>
+          )} 
       </div>
     </section>
   )
